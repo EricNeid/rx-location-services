edd3a299ed105369625decc8fbc4858c82b9ad98
Eric Neidhardt <eric.neidhardt@gmail.com>
2019-04-05 11:05:38 +0200

    Added more specific exceptions for missing permissions

---
+++ rx-location-service/src/main/kotlin/org/neidhardt/rxlocation/exceptions/MissingPermission.kts
@@ -0,0 +1,16 @@
+package org.neidhardt.rxlocation
+
+/**
+ * MissingPermissionFineLocation indicates that permission Manifest.permission.ACCESS_FINE_LOCATION
+ * is missing and required for the operation.
+ *
+ * @param message - detailed explanation of error cause
+ */
+class MissingPermissionFineLocation(message: String?) : Throwable(message, null)
+/**
+ * MissingPermissionFineLocation indicates that permission Manifest.permission.ACCESS_COARSE_LOCATION
+ * is missing and required for the operation.
+ *
+ * @param message - detailed explanation of error cause
+ */
+class MissingPermissionCoarseLocation(message: String?) : Throwable(message, null)
