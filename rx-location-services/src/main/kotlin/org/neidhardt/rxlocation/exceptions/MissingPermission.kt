6ae7218b706cc6b47c6f556d99da21159a19dc35
Eric Neidhardt <eric.neidhardt@gmail.com>
2020-04-06 10:59:49 +0200

    Update readme

--- rx-location-services/src/main/kotlin/org/neidhardt/rxlocation/exceptions/MissingPermission.kt
+++ rx-location-services/src/main/kotlin/org/neidhardt/rxlocation/exceptions/MissingPermission.kt
@@ -1,14 +1,15 @@
 package org.neidhardt.rxlocation.exceptions
 
 /**
- * MissingPermissionFineLocation indicates that permission Manifest.permission.ACCESS_FINE_LOCATION
+ * [MissingPermissionFineLocation] indicates that permission Manifest.permission.ACCESS_FINE_LOCATION
  * is missing and required for the operation.
  *
  * @param message - detailed explanation of error cause
  */
 class MissingPermissionFineLocation(message: String?) : Throwable(message, null)
+
 /**
- * MissingPermissionFineLocation indicates that permission Manifest.permission.ACCESS_COARSE_LOCATION
+ * [MissingPermissionFineLocation] indicates that permission Manifest.permission.ACCESS_COARSE_LOCATION
  * is missing and required for the operation.
  *
  * @param message - detailed explanation of error cause
--- rx-location-services/src/main/kotlin/org/neidhardt/rxlocation/exceptions/MissingSensor.kt
+++ rx-location-services/src/main/kotlin/org/neidhardt/rxlocation/exceptions/MissingSensor.kt
@@ -1,7 +1,7 @@
 package org.neidhardt.rxlocation.exceptions
 
 /**
- * MissingSensor indicates that one of the required sensors for this operation could not be accessed.
+ * [MissingSensor] indicates that one of the required sensors for this operation could not be accessed.
  * This is a rare error an could mean that the phone is missing the required hardware.
  *
  * @param message - detailed explanation of error cause
