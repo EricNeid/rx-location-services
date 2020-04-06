43aa406adc8c8ecc9ea8635e26515d8b6a40d3ba
Eric Neidhardt <eric.neidhardt@gmail.com>
2020-04-06 12:40:36 +0200

    Added test for android location service

---
+++ rx-location-services/src/androidTest/java/org/neidhardt/rxlocation/services/AndroidLocationServiceTest.kt
@@ -0,0 +1,50 @@
+package org.neidhardt.rxlocation.services
+
+import android.content.Context
+import android.location.LocationManager
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+
+@LargeTest
+class AndroidLocationServiceTest {
+
+	@Rule
+	@JvmField
+	val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+			android.Manifest.permission.ACCESS_FINE_LOCATION,
+			android.Manifest.permission.ACCESS_COARSE_LOCATION,
+			android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+	)
+
+	private lateinit var context: Context
+	private lateinit var unit: AndroidLocationService
+
+	@Before
+	fun setUp() {
+		this.context = InstrumentationRegistry.getInstrumentation().context
+		this.unit = AndroidLocationService(context)
+	}
+
+	@Test
+	fun getLocationUpdates() {
+		// action
+		val result = unit.getLocationUpdates(
+				1000,
+				1f,
+				LocationManager.GPS_PROVIDER
+		).blockingFirst()
+		// verify
+		assertNotNull(result)
+		assertNotNull(result.latitude)
+		assertNotNull(result.longitude)
+		assertEquals(result, unit.lastKnowLocation)
+	}
+
+}
